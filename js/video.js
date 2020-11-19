93472561a49b13fd683b26718bc53a4af54e7046
Ashley Beals <bealsa@umich.edu>
2020-11-19 16:47:48 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -6,9 +6,30 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play();
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	console.log("Pause Video");
+	video.pause();
 });
 
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	video.playbackRate -= .10;
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up");
+	video.playbackRate += (1 - video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Ahead");
+	video.pause();
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute");
+	video.muted = true;
+});
